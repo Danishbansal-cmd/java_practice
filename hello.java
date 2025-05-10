df91fc008068ba73783cf1e4198e1bb6cce63dc4
Danish Bansal <danishbansal60@gmail.com>
2025-05-10 19:31:37 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+class hello {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        PrintStream out = new PrintStream();
+        out.println("the number is: " + n);
+
+    }
+}
